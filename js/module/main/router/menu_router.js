c9d792e6e37ec738bcf67e3c2ea0db58d11467d3
lichenglong <lichenglong@wanglibank.com>
2017-06-14 13:48:10 +0800

    增信页背景修改

--- js/module/main/router/menu_router.js
+++ js/module/main/router/menu_router.js
@@ -1,6 +1,6 @@
 ;
 HJY.config(["$stateProvider", "$urlRouterProvider", "$ionicConfigProvider", "$locationProvider", "$httpProvider", function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $locationProvider, $httpProvider) {
-    window.version_glo = "4.1";
+    window.version_glo = "4.2";
     $ionicConfigProvider.views.maxCache(5);
     var v = "?" + window.version_glo;
 
